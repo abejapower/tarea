fa17697475b818aef07bd092fad6f4467382840a
Nicole Elgueta <nicole.m.elgueta@gmail.com>
2018-05-29 02:28:20 +0200

    tarea

---
+++ app.js
@@ -0,0 +1,20 @@
+window.onload= ()=>{
+    /*ceviche,tacos al pastor, empanada chilena, hamburguesa */
+const menu = ("¿que quieres comer?")
+switch (menu){
+    case "ceviche" :
+    alert(ceviche);
+    break;
+
+    case " tacos al pastor" :
+    alert(tacos) ;
+    break;
+
+    case " empanadas chilenas" :
+    alert(empanadas);
+
+    default :
+    alert("hamburguesas");
+ }
+}
+
